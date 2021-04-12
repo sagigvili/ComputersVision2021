723c33e7a7a2e112978f9e1de2a7cf3bb0862c6e
Orash94 <orash94@gmail.com>
2021-04-12 20:25:39 +0300

    fixed exerecise A in the SIFT assignment
    (works af)

--- Assignment1/SIFT/exeA.py
+++ Assignment1/SIFT/exeA.py
@@ -10,19 +10,19 @@ count = 1
 for kp in kp1:
     if count % 5 == 0:
         count += 1
-        pass
+        continue
     else:
-        angle = kp.angle
-        size = kp.size
-        x_cord = round(kp.pt[0])
-        y_cord = round(kp.pt[1])
-        x1 = round(kp.pt[0] + (kp.size * np.cos(kp.angle)))
-        y1 = round(kp.pt[1] + kp.size * np.sin(kp.angle))
-        cv2.arrowedLine(img1,(x_cord,y_cord),(x1,y1),(255,0,0), 1)
+        angle = np.deg2rad(kp.angle)
+        size = round(kp.size / 2)
+        x_cord = kp.pt[0]
+        y_cord = kp.pt[1]
+        x1 = round(kp.pt[0] + (size * np.cos(angle)))
+        y1 = round(kp.pt[1] + (size * np.sin(angle)))
+        cv2.arrowedLine(img1, (round(x_cord), round(y_cord)), (x1, y1), (255, 0, 0), 1)
+
         count += 1
 
-
-img3 = cv2.drawKeypoints(img1,kp1, None, (0,0,255))
+img3 = cv2.drawKeypoints(img1, kp1, None, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 cv2.imshow("Key Points as simon wants", img1)
 cv2.imshow("Key Points that simon doesn't approve", img3)
 cv2.waitKey(0)
