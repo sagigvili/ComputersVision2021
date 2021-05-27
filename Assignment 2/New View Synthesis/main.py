e83d768529222775dc0dd97b8e44958847baa232
Orash94 <orash94@gmail.com>
2021-05-27 15:29:21 +0300

    2D-3D-2D almost work perfect

---
+++ Assignment 2/New View Synthesis/main.py
@@ -0,0 +1,86 @@
+import numpy as np
+import cv2
+import cv_io
+
+depthMapAlley = cv_io.sintel_io.depth_read('alley_2.dpt')
+# cv_io.sintel_io.depth_write('test.dpt', image)     Creating depth map
+IntrinsicAlley, ExtrinsicAlley = cv_io.sintel_io.cam_read('alley_2.cam')
+img1 = cv2.imread("alley_2.png")
+r, c, e = np.shape(img1)
+inverseIntrinsic = np.linalg.inv(IntrinsicAlley)
+cam_points = np.float64(np.zeros((r * c, 3)))
+i = 0
+# Loop through each pixel in the image
+for v in range(r):
+    for u in range(c):
+        #Apply equation in fig 5
+        x = (u - IntrinsicAlley[0][2]) * depthMapAlley[v, u] / IntrinsicAlley[0][0]
+        y = (v - IntrinsicAlley[1][2]) * depthMapAlley[v, u] / IntrinsicAlley[1][1]
+        z = depthMapAlley[v, u]
+        cam_points[i] = (x, y, z)
+        i = i + 1
+
+backprojectionMatrix = np.float32(np.zeros((r * c, 2)))
+homogenouos3DCoords = np.float32(np.ones((r * c, 4)))
+for i in range(r):
+    homogenouos3DCoords[i][0] = cam_points[i][0]
+    homogenouos3DCoords[i][1] = cam_points[i][1]
+    homogenouos3DCoords[i][2] = cam_points[i][2]
+
+newExtrinsic = np.zeros((3,4))
+newExtrinsic[0][0] = 1
+newExtrinsic[1][1] = 1
+newExtrinsic[2][2] = 1
+# temp = np.ones((1, 4))
+# ExtrinsicAlley = np.vstack((ExtrinsicAlley, temp))
+# cam_mat = np.matmul(IntrinsicAlley, np.matmul(newExtrinsic, ExtrinsicAlley))
+# img_cords_Homogeuous = np.matmul(cam_mat, np.transpose(homogenouos3DCoords))
+
+# homogBackProj = np.matmul(np.matmul(IntrinsicAlley, ExtrinsicAlley), np.transpose(homogenouos3DCoords))
+
+
+
+'''
+THAT'S WORKING
+BUT I NEED TO TRY IT WITH THE EXTRINSIC MATRIX
+'''
+for i in range(r*c):
+    u1 = int(np.round((cam_points[i][0] * IntrinsicAlley[0][0] / cam_points[i][2]) + IntrinsicAlley[0][2]))
+    v1 = int(np.round((cam_points[i][1] * IntrinsicAlley[1][1] / cam_points[i][2]) + IntrinsicAlley[1][2]))
+    backprojectionMatrix[i] = (u1, v1)
+
+
+
+
+# #homogBackProj = np.transpose(homogBackProj)
+# k = 0
+# for i in range(backprojectionMatrix.shape[1]):
+#     backprojectionMatrix[0][i] = int(np.round(img_cords_Homogeuous[0][i] / img_cords_Homogeuous[2][i]))
+#     backprojectionMatrix[1][i] = int(np.round(img_cords_Homogeuous[1][i] / img_cords_Homogeuous[2][i]))
+#     k = k+1
+
+
+
+
+newIm = np.zeros(np.shape(img1))
+#backprojectionMatrix = np.transpose(backprojectionMatrix)
+k = 0
+for i in range(r):
+    for j in range(c):
+        m, n = backprojectionMatrix[k]
+        if m>= c or n >= r or m<0 or n < 0:
+            pass
+        else:
+            print("i = " + str(i) + " , j = "+str(j))
+            print("m = " + str(m) + " , n = " + str(n))
+            newIm[i][j] = img1[int(n)][int(m)]
+        k = k+1
+        # newIm[i][j] = img1[int(m)][int(n)]
+        # newIm[i][j] = img1[int(m)][int(n)]
+
+
+
+#cv_io.imshow(depthMapAlley)
+cv2.imshow("origin", img1)
+cv2.imshow("2D-3D-2D", newIm / 256)
+cv2.waitKey(0)
--- Assignment 2/Photometric Stereo/SumSquareDifferences.py
+++ Assignment 2/Photometric Stereo/SumSquareDifferences.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
 from pylab import *
-
+import copy
+import signal as sig
 
 img1 = cv2.imread("Resources/im_left.png")
 img2 = cv2.imread("Resources/im_right.png")
@@ -10,7 +11,8 @@ img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 # im = np.vstack((img1, img2))
 DisparityIm = np.zeros(np.shape(img1))
-k = 15
+DisparityIm = np.float64(DisparityIm)
+k = 9
 radius = int(np.floor(k / 2))
 rows = img1.shape[0]
 cols = img1.shape[1]
@@ -22,16 +24,11 @@ for i in range(rows):
         bottom = i + radius +1
         if left < 0 or right > cols or top < 0 or bottom > rows:
             continue
-        # if left < 0:
-        #     left = 0
-        # if right > cols:
-        #     right = cols
-        # if top < 0:
-        #     top = 0
-        # if bottom > rows:
-        #     bottom = rows
-        # else:
-        template = img1[top:bottom, left:right]
+
+        template = copy.deepcopy(img1[top:bottom, left:right])
+        template = np.float64(template)
+        #scanLineTemplate = img2[top:bottom, 0:cols]
+
         # for m in range(cols):
         #     scanLineTemplate = img2[top:bottom, m:cols]
         min_val = np.inf
@@ -42,19 +39,24 @@ for i in range(rows):
             if leftTemp < 0 or rightTemp > cols:
                 continue
             else:
-                scanLineTemplate = img2[top:bottom, leftTemp:rightTemp]
-                SSD = np.sum((scanLineTemplate - template)**2)
-                # a = np.matrix.flatten(template)
-                # b = np.matrix.flatten(scanLineTemplate)
-                # normA = a / np.linalg.norm(a)
-                # normB = b / np.linalg.norm(b)
-                # NCC = np.dot(a, b) / np.dot(normA, normB)
+                scanLineTemplate = np.zeros((k, k))
+                scanLineTemplate = copy.deepcopy(img2[top:bottom, leftTemp:rightTemp])
+                scanLineTemplate = np.float64(scanLineTemplate)
+                SSD = np.sum(np.subtract(scanLineTemplate, template) * np.subtract(scanLineTemplate, template))
+                #SSD = cv2.norm((np.matrix.flatten(scanLineTemplate) - np.matrix.flatten(template)), cv2.NORM_L2)
+                # tmpMat = np.zeros((k,k))
+                # for u in range(k):
+                #     for v in range(k):
+                #         res1 = scanLineTemplate[u][v] - template[u][v]
+                #         res1 = res1**2
+                #         tmpMat[u][v] = res1
+                # SSD = np.sum(tmpMat)
                 if SSD < min_val:
                     min_val = SSD
                     minLocation = m
         if minLocation == j:
             minLocation = minLocation + 0.000000001
-        DisparityIm[i][j] = 20/cv2.norm((minLocation - j), cv2.NORM_L2)
+        DisparityIm[i][j] = abs(j - minLocation)
 
         # minDist = np.inf
         # temp = minLocation[1]
@@ -75,12 +77,15 @@ for i in range(rows):
 # cv2.imshow("sasi 1", img1)
 # newIm = np.zeros(np.shape(DisparityIm))
 # cv2.normalize(DisparityIm, newIm, 0, 1, cv2.NORM_MINMAX)
-DisparityIm = 1 - DisparityIm
-cv2.imshow("sasi 2", DisparityIm)
+
+DisparityImTemp = 1 - DisparityIm
+cv2.imshow("sasi SSD", (1 - (DisparityIm / 256)))
 cv2.waitKey(0)
 
 groundTruthIm = cv2.imread("Resources/disp_left.png")
 groundTruthIm = cv2.cvtColor(groundTruthIm, cv2.COLOR_BGR2GRAY)
+groundTruthIm = np.float32(groundTruthIm)
+groundTruthIm = groundTruthIm / 3
 
 '''
 1 - mean of absolute differences
@@ -88,42 +93,42 @@ groundTruthIm = cv2.cvtColor(groundTruthIm, cv2.COLOR_BGR2GRAY)
 3 - Bad 0.5
 4 - Bad 4
 '''
-measureType = 3
+measureType = 1
 
-if measureType == 1:
-    temp = DisparityIm / 20
-    temp = np.round(((temp + 1) / 2 ) * 255)
-    res = np.mean(np.absolute(temp - groundTruthIm))
-    print("the mean of absolute differences is: " + str(res))
+#if measureType == 1:
+temp = DisparityIm
+#temp = np.round(((temp + 1) / 2 ) * 255)
+res = np.mean(np.absolute(temp - groundTruthIm))
+print("the mean of absolute differences is: " + str(res))
 
-elif measureType == 2:
-    temp = DisparityIm / 20
-    temp = np.round(((temp + 1) / 2 ) * 255)
-    res = np.median(np.absolute(temp - groundTruthIm))
-    print("the median of absolute differences is: " + str(res))
+#elif measureType == 2:
+temp = DisparityIm
+#temp = np.round(((temp + 1) / 2 ) * 255)
+res = np.median(np.absolute(temp - groundTruthIm))
+print("the median of absolute differences is: " + str(res))
 
-elif measureType == 3:
-    temp = DisparityIm / 20
-    temp = np.round(((temp + 1) / 2) * 255)
-    res = np.absolute(temp - groundTruthIm)
-    count = 0
-    for i in range(rows):
-        for j in range(cols):
-            if res[i][j] > 0.5:
-                count = count + 1
-    res = (count / np.size(groundTruthIm)) * 100
-    print("the bad0.5 is: " + str(res))
+#elif measureType == 3:
+temp = DisparityIm
+#temp = np.round(((temp + 1) / 2) * 255)
+res = np.absolute(temp - groundTruthIm)
+count = 0
+for i in range(rows):
+    for j in range(cols):
+        if res[i][j] > 0.5:
+            count = count + 1
+res = (count / np.size(groundTruthIm)) * 100
+print("the bad0.5 is: " + str(res))
 
 
-elif measureType == 4:
-    temp = DisparityIm / 20
-    temp = np.round(((temp + 1) / 2) * 255)
-    res = np.absolute(temp - groundTruthIm)
-    count = 0
-    for i in range(rows):
-        for j in range(cols):
-            if res[i][j] > 4:
-                count = count + 1
-    res = (count / np.size(groundTruthIm)) * 100
-    print("the bad4 is: " + str(res))
+#elif measureType == 4:
+temp = DisparityIm
+#temp = np.round(((temp + 1) / 2) * 255)
+res = np.absolute(temp - groundTruthIm)
+count = 0
+for i in range(rows):
+    for j in range(cols):
+        if res[i][j] > 4:
+            count = count + 1
+res = (count / np.size(groundTruthIm)) * 100
+print("the bad4 is: " + str(res))
 
