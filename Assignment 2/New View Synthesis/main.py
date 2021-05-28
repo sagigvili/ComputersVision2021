43c42506646be36e03776ed0e326c6f73e248959
Orash94 <orash94@gmail.com>
2021-05-28 15:30:22 +0300

    camera pose estimation almost working

--- Assignment 2/New View Synthesis/main.py
+++ Assignment 2/New View Synthesis/main.py
@@ -3,6 +3,29 @@ import cv2
 import cv_io
 
 
+# Calculates Rotation Matrix given euler angles.
+def eulerAnglesToRotationMatrix(thetaX=0, thetaY=0, thetaZ=0):
+    thetaX = np.deg2rad(thetaX)
+    thetaY = np.deg2rad(thetaY)
+    thetaZ = np.deg2rad(thetaZ)
+    Rx = np.array([[1, 0, 0],
+                   [0, np.cos(thetaX), -np.sin(thetaX)],
+                   [0, np.sin(thetaX), np.cos(thetaX)]
+                   ])
+
+    Ry = np.array([[np.cos(thetaY), 0, np.sin(thetaY)],
+                   [0, 1, 0],
+                   [-np.sin(thetaY), 0, np.cos(thetaY)]
+                   ])
+
+    Rz = np.array([[np.cos(thetaZ), -np.sin(thetaZ), 0],
+                   [np.sin(thetaZ), np.cos(thetaZ), 0],
+                   [0, 0, 1]
+                   ])
+
+    R = np.dot(Rz, np.dot(Ry, Rx))
+
+    return R
 
 
 depthMapAlley = cv_io.sintel_io.depth_read('alley_2.dpt')
@@ -27,45 +50,380 @@ D3_points[:, 0] = m1[:, 0] * np.matrix.flatten(depthMapAlley)
 D3_points[:, 1] = m1[:, 1] * np.matrix.flatten(depthMapAlley)
 D3_points[:, 2] = m1[:, 2] * np.matrix.flatten(depthMapAlley)
 
-
-
-backprojectionMatrix = np.float32(np.zeros((2, r * c)))
+# backprojectionMatrix = np.float32(np.zeros((2, r * c)))
 homogenouos3DCoords = np.float32(np.ones((r * c, 4)))
-for i in range(r*c):
+for i in range(r * c):
     homogenouos3DCoords[i][0] = D3_points[i][0]
     homogenouos3DCoords[i][1] = D3_points[i][1]
     homogenouos3DCoords[i][2] = D3_points[i][2]
 
-newExtrinsic = np.eye(3, 4)
-cam_mat = IntrinsicAlley @ newExtrinsic
-img_cords_Homogenuous = cam_mat @ np.transpose(homogenouos3DCoords)
 
-for i in range(r * c):
-    backprojectionMatrix[0][i] = int(np.round(img_cords_Homogenuous[0][i] / img_cords_Homogenuous[2][i]))
-    backprojectionMatrix[1][i] = int(np.round(img_cords_Homogenuous[1][i] / img_cords_Homogenuous[2][i]))
+angle = 0
+imNumber = 0
+for angle in np.arange(0, 5, 0.05):
+    backprojectionMatrix = np.float32(np.zeros((2, r * c)))
+    T = np.array([[0], [angle], [0]])
+    R = eulerAnglesToRotationMatrix(0, 0, 0)
+    newExtrinsic = np.hstack((R, T))
+    cam_mat = IntrinsicAlley @ newExtrinsic
+    img_cords_Homogenuous = cam_mat @ np.transpose(homogenouos3DCoords)
+
+    for i in range(r * c):
+        backprojectionMatrix[0][i] = int(np.round(img_cords_Homogenuous[0][i] / img_cords_Homogenuous[2][i]))
+        backprojectionMatrix[1][i] = int(np.round(img_cords_Homogenuous[1][i] / img_cords_Homogenuous[2][i]))
+
+    newIm = np.zeros(np.shape(img1))
+    backprojectionMatrix = np.transpose(backprojectionMatrix)
+    k = 0
+    for i in range(r):
+        for j in range(c):
+            m, n = backprojectionMatrix[k]
+            if n >= c or m >= r or n < 0 or m < 0:
+                pass
+            else:
+                newIm[i][j] = img1[int(m)][int(n)]
+            k = k + 1
+
+    # cv_io.imshow(depthMapAlley)
+    #cv2.imshow("origin", img1)
+    cv2.imwrite("im  test " + str(imNumber)+".jpg", newIm)
+    print("image  number: " + str(imNumber))
+    imNumber = imNumber + 1
 
 
-
-newIm = np.zeros(np.shape(img1))
-backprojectionMatrix = np.transpose(backprojectionMatrix)
-k = 0
-for i in range(r):
-    for j in range(c):
-        m, n = backprojectionMatrix[k]
-        if n >= c or m >= r or n < 0 or m < 0:
-            pass
-        else:
-            print("i = " + str(i) + " , j = " + str(j))
-            print("m = " + str(m) + " , n = " + str(n))
-            newIm[i][j] = img1[int(m)][int(n)]
-        k = k + 1
-
-
-# cv_io.imshow(depthMapAlley)
-cv2.imshow("origin", img1)
-cv2.imshow("2D-3D-2D", newIm / 256)
-cv2.waitKey(0)
-
-
-
-
+# for angle in np.arange(0, 15, 0.5):
+#     backprojectionMatrix = np.float32(np.zeros((2, r * c)))
+#     T = np.array([[0], [0], [0]])
+#     R = eulerAnglesToRotationMatrix(15 - angle, 0, 0)
+#     newExtrinsic = np.hstack((R, T))
+#     cam_mat = IntrinsicAlley @ newExtrinsic
+#     img_cords_Homogenuous = cam_mat @ np.transpose(homogenouos3DCoords)
+#
+#     for i in range(r * c):
+#         backprojectionMatrix[0][i] = int(np.round(img_cords_Homogenuous[0][i] / img_cords_Homogenuous[2][i]))
+#         backprojectionMatrix[1][i] = int(np.round(img_cords_Homogenuous[1][i] / img_cords_Homogenuous[2][i]))
+#
+#     newIm = np.zeros(np.shape(img1))
+#     backprojectionMatrix = np.transpose(backprojectionMatrix)
+#     k = 0
+#     for i in range(r):
+#         for j in range(c):
+#             m, n = backprojectionMatrix[k]
+#             if n >= c or m >= r or n < 0 or m < 0:
+#                 pass
+#             else:
+#                 newIm[i][j] = img1[int(m)][int(n)]
+#             k = k + 1
+#
+#     # cv_io.imshow(depthMapAlley)
+#     #cv2.imshow("origin", img1)
+#     cv2.imwrite("im " + str(imNumber)+".jpg", newIm)
+#     print("image number: " + str(imNumber))
+#     imNumber = imNumber + 1
+#
+# for angle in np.arange(0, 15, 0.5):
+#     backprojectionMatrix = np.float32(np.zeros((2, r * c)))
+#     T = np.array([[0], [0], [0]])
+#     R = eulerAnglesToRotationMatrix(-angle, 0, 0)
+#     newExtrinsic = np.hstack((R, T))
+#     cam_mat = IntrinsicAlley @ newExtrinsic
+#     img_cords_Homogenuous = cam_mat @ np.transpose(homogenouos3DCoords)
+#
+#     for i in range(r * c):
+#         backprojectionMatrix[0][i] = int(np.round(img_cords_Homogenuous[0][i] / img_cords_Homogenuous[2][i]))
+#         backprojectionMatrix[1][i] = int(np.round(img_cords_Homogenuous[1][i] / img_cords_Homogenuous[2][i]))
+#
+#     newIm = np.zeros(np.shape(img1))
+#     backprojectionMatrix = np.transpose(backprojectionMatrix)
+#     k = 0
+#     for i in range(r):
+#         for j in range(c):
+#             m, n = backprojectionMatrix[k]
+#             if n >= c or m >= r or n < 0 or m < 0:
+#                 pass
+#             else:
+#                 newIm[i][j] = img1[int(m)][int(n)]
+#             k = k + 1
+#
+#     # cv_io.imshow(depthMapAlley)
+#     #cv2.imshow("origin", img1)
+#     cv2.imwrite("im " + str(imNumber)+".jpg", newIm)
+#     print("image number: " + str(imNumber))
+#     imNumber = imNumber + 1
+#
+#
+# for angle in np.arange(0, 15, 0.5):
+#     backprojectionMatrix = np.float32(np.zeros((2, r * c)))
+#     T = np.array([[0], [0], [0]])
+#     R = eulerAnglesToRotationMatrix(angle - 15, 0, 0)
+#     newExtrinsic = np.hstack((R, T))
+#     cam_mat = IntrinsicAlley @ newExtrinsic
+#     img_cords_Homogenuous = cam_mat @ np.transpose(homogenouos3DCoords)
+#
+#     for i in range(r * c):
+#         backprojectionMatrix[0][i] = int(np.round(img_cords_Homogenuous[0][i] / img_cords_Homogenuous[2][i]))
+#         backprojectionMatrix[1][i] = int(np.round(img_cords_Homogenuous[1][i] / img_cords_Homogenuous[2][i]))
+#
+#     newIm = np.zeros(np.shape(img1))
+#     backprojectionMatrix = np.transpose(backprojectionMatrix)
+#     k = 0
+#     for i in range(r):
+#         for j in range(c):
+#             m, n = backprojectionMatrix[k]
+#             if n >= c or m >= r or n < 0 or m < 0:
+#                 pass
+#             else:
+#                 newIm[i][j] = img1[int(m)][int(n)]
+#             k = k + 1
+#
+#     # cv_io.imshow(depthMapAlley)
+#     #cv2.imshow("origin", img1)
+#     cv2.imwrite("im " + str(imNumber)+".jpg", newIm)
+#     print("image number: " + str(imNumber))
+#     imNumber = imNumber + 1
+#
+# for angle in np.arange(0, 15, 0.5):
+#     backprojectionMatrix = np.float32(np.zeros((2, r * c)))
+#     T = np.array([[0], [0], [0]])
+#     R = eulerAnglesToRotationMatrix(0, angle, 0)
+#     newExtrinsic = np.hstack((R, T))
+#     cam_mat = IntrinsicAlley @ newExtrinsic
+#     img_cords_Homogenuous = cam_mat @ np.transpose(homogenouos3DCoords)
+#
+#     for i in range(r * c):
+#         backprojectionMatrix[0][i] = int(np.round(img_cords_Homogenuous[0][i] / img_cords_Homogenuous[2][i]))
+#         backprojectionMatrix[1][i] = int(np.round(img_cords_Homogenuous[1][i] / img_cords_Homogenuous[2][i]))
+#
+#     newIm = np.zeros(np.shape(img1))
+#     backprojectionMatrix = np.transpose(backprojectionMatrix)
+#     k = 0
+#     for i in range(r):
+#         for j in range(c):
+#             m, n = backprojectionMatrix[k]
+#             if n >= c or m >= r or n < 0 or m < 0:
+#                 pass
+#             else:
+#                 newIm[i][j] = img1[int(m)][int(n)]
+#             k = k + 1
+#
+#     # cv_io.imshow(depthMapAlley)
+#     # cv2.imshow("origin", img1)
+#     cv2.imwrite("im " + str(imNumber) + ".jpg", newIm)
+#     print("image number: " + str(imNumber))
+#     imNumber = imNumber + 1
+#
+# for angle in np.arange(0, 15, 0.5):
+#     backprojectionMatrix = np.float32(np.zeros((2, r * c)))
+#     T = np.array([[0], [0], [0]])
+#     R = eulerAnglesToRotationMatrix(0, 15 - angle, 0)
+#     newExtrinsic = np.hstack((R, T))
+#     cam_mat = IntrinsicAlley @ newExtrinsic
+#     img_cords_Homogenuous = cam_mat @ np.transpose(homogenouos3DCoords)
+#
+#     for i in range(r * c):
+#         backprojectionMatrix[0][i] = int(np.round(img_cords_Homogenuous[0][i] / img_cords_Homogenuous[2][i]))
+#         backprojectionMatrix[1][i] = int(np.round(img_cords_Homogenuous[1][i] / img_cords_Homogenuous[2][i]))
+#
+#     newIm = np.zeros(np.shape(img1))
+#     backprojectionMatrix = np.transpose(backprojectionMatrix)
+#     k = 0
+#     for i in range(r):
+#         for j in range(c):
+#             m, n = backprojectionMatrix[k]
+#             if n >= c or m >= r or n < 0 or m < 0:
+#                 pass
+#             else:
+#                 newIm[i][j] = img1[int(m)][int(n)]
+#             k = k + 1
+#
+#     # cv_io.imshow(depthMapAlley)
+#     # cv2.imshow("origin", img1)
+#     cv2.imwrite("im " + str(imNumber) + ".jpg", newIm)
+#     print("image number: " + str(imNumber))
+#     imNumber = imNumber + 1
+#
+#
+# for angle in np.arange(0, 15, 0.5):
+#     backprojectionMatrix = np.float32(np.zeros((2, r * c)))
+#     T = np.array([[0], [0], [0]])
+#     R = eulerAnglesToRotationMatrix(0, -angle, 0)
+#     newExtrinsic = np.hstack((R, T))
+#     cam_mat = IntrinsicAlley @ newExtrinsic
+#     img_cords_Homogenuous = cam_mat @ np.transpose(homogenouos3DCoords)
+#
+#     for i in range(r * c):
+#         backprojectionMatrix[0][i] = int(np.round(img_cords_Homogenuous[0][i] / img_cords_Homogenuous[2][i]))
+#         backprojectionMatrix[1][i] = int(np.round(img_cords_Homogenuous[1][i] / img_cords_Homogenuous[2][i]))
+#
+#     newIm = np.zeros(np.shape(img1))
+#     backprojectionMatrix = np.transpose(backprojectionMatrix)
+#     k = 0
+#     for i in range(r):
+#         for j in range(c):
+#             m, n = backprojectionMatrix[k]
+#             if n >= c or m >= r or n < 0 or m < 0:
+#                 pass
+#             else:
+#                 newIm[i][j] = img1[int(m)][int(n)]
+#             k = k + 1
+#
+#     # cv_io.imshow(depthMapAlley)
+#     # cv2.imshow("origin", img1)
+#     cv2.imwrite("im " + str(imNumber) + ".jpg", newIm)
+#     print("image number: " + str(imNumber))
+#     imNumber = imNumber + 1
+#
+#
+# for angle in np.arange(0, 15, 0.5):
+#     backprojectionMatrix = np.float32(np.zeros((2, r * c)))
+#     T = np.array([[0], [0], [0]])
+#     R = eulerAnglesToRotationMatrix(0, angle - 15, 0)
+#     newExtrinsic = np.hstack((R, T))
+#     cam_mat = IntrinsicAlley @ newExtrinsic
+#     img_cords_Homogenuous = cam_mat @ np.transpose(homogenouos3DCoords)
+#
+#     for i in range(r * c):
+#         backprojectionMatrix[0][i] = int(np.round(img_cords_Homogenuous[0][i] / img_cords_Homogenuous[2][i]))
+#         backprojectionMatrix[1][i] = int(np.round(img_cords_Homogenuous[1][i] / img_cords_Homogenuous[2][i]))
+#
+#     newIm = np.zeros(np.shape(img1))
+#     backprojectionMatrix = np.transpose(backprojectionMatrix)
+#     k = 0
+#     for i in range(r):
+#         for j in range(c):
+#             m, n = backprojectionMatrix[k]
+#             if n >= c or m >= r or n < 0 or m < 0:
+#                 pass
+#             else:
+#                 newIm[i][j] = img1[int(m)][int(n)]
+#             k = k + 1
+#
+#     # cv_io.imshow(depthMapAlley)
+#     # cv2.imshow("origin", img1)
+#     cv2.imwrite("im " + str(imNumber) + ".jpg", newIm)
+#     print("image number: " + str(imNumber))
+#     imNumber = imNumber + 1
+#
+#
+# for angle in np.arange(0, 15, 0.5):
+#     backprojectionMatrix = np.float32(np.zeros((2, r * c)))
+#     T = np.array([[0], [0], [0]])
+#     R = eulerAnglesToRotationMatrix(0, 0, angle)
+#     newExtrinsic = np.hstack((R, T))
+#     cam_mat = IntrinsicAlley @ newExtrinsic
+#     img_cords_Homogenuous = cam_mat @ np.transpose(homogenouos3DCoords)
+#
+#     for i in range(r * c):
+#         backprojectionMatrix[0][i] = int(np.round(img_cords_Homogenuous[0][i] / img_cords_Homogenuous[2][i]))
+#         backprojectionMatrix[1][i] = int(np.round(img_cords_Homogenuous[1][i] / img_cords_Homogenuous[2][i]))
+#
+#     newIm = np.zeros(np.shape(img1))
+#     backprojectionMatrix = np.transpose(backprojectionMatrix)
+#     k = 0
+#     for i in range(r):
+#         for j in range(c):
+#             m, n = backprojectionMatrix[k]
+#             if n >= c or m >= r or n < 0 or m < 0:
+#                 pass
+#             else:
+#                 newIm[i][j] = img1[int(m)][int(n)]
+#             k = k + 1
+#
+#     # cv_io.imshow(depthMapAlley)
+#     # cv2.imshow("origin", img1)
+#     cv2.imwrite("im " + str(imNumber) + ".jpg", newIm)
+#     # cv2.waitKey(0)
+#     print("image number: " + str(imNumber))
+#     imNumber = imNumber + 1
+#
+#
+#
+# for angle in np.arange(0, 15, 0.5):
+#     backprojectionMatrix = np.float32(np.zeros((2, r * c)))
+#     T = np.array([[0], [0], [0]])
+#     R = eulerAnglesToRotationMatrix(0, 0, 15 - angle)
+#     newExtrinsic = np.hstack((R, T))
+#     cam_mat = IntrinsicAlley @ newExtrinsic
+#     img_cords_Homogenuous = cam_mat @ np.transpose(homogenouos3DCoords)
+#
+#     for i in range(r * c):
+#         backprojectionMatrix[0][i] = int(np.round(img_cords_Homogenuous[0][i] / img_cords_Homogenuous[2][i]))
+#         backprojectionMatrix[1][i] = int(np.round(img_cords_Homogenuous[1][i] / img_cords_Homogenuous[2][i]))
+#
+#     newIm = np.zeros(np.shape(img1))
+#     backprojectionMatrix = np.transpose(backprojectionMatrix)
+#     k = 0
+#     for i in range(r):
+#         for j in range(c):
+#             m, n = backprojectionMatrix[k]
+#             if n >= c or m >= r or n < 0 or m < 0:
+#                 pass
+#             else:
+#                 newIm[i][j] = img1[int(m)][int(n)]
+#             k = k + 1
+#
+#     # cv_io.imshow(depthMapAlley)
+#     # cv2.imshow("origin", img1)
+#     cv2.imwrite("im " + str(imNumber) + ".jpg", newIm)
+#     print("image number: " + str(imNumber))
+#     imNumber = imNumber + 1
+#
+# for angle in np.arange(0, 15, 0.5):
+#     backprojectionMatrix = np.float32(np.zeros((2, r * c)))
+#     T = np.array([[0], [0], [0]])
+#     R = eulerAnglesToRotationMatrix(0, 0, -angle)
+#     newExtrinsic = np.hstack((R, T))
+#     cam_mat = IntrinsicAlley @ newExtrinsic
+#     img_cords_Homogenuous = cam_mat @ np.transpose(homogenouos3DCoords)
+#
+#     for i in range(r * c):
+#         backprojectionMatrix[0][i] = int(np.round(img_cords_Homogenuous[0][i] / img_cords_Homogenuous[2][i]))
+#         backprojectionMatrix[1][i] = int(np.round(img_cords_Homogenuous[1][i] / img_cords_Homogenuous[2][i]))
+#
+#     newIm = np.zeros(np.shape(img1))
+#     backprojectionMatrix = np.transpose(backprojectionMatrix)
+#     k = 0
+#     for i in range(r):
+#         for j in range(c):
+#             m, n = backprojectionMatrix[k]
+#             if n >= c or m >= r or n < 0 or m < 0:
+#                 pass
+#             else:
+#                 newIm[i][j] = img1[int(m)][int(n)]
+#             k = k + 1
+#
+#     # cv_io.imshow(depthMapAlley)
+#     # cv2.imshow("origin", img1)
+#     cv2.imwrite("im " + str(imNumber) + ".jpg", newIm)
+#     print("image number: " + str(imNumber))
+#     imNumber = imNumber + 1
+#
+# for angle in np.arange(0, 15, 0.5):
+#     backprojectionMatrix = np.float32(np.zeros((2, r * c)))
+#     T = np.array([[0], [0], [0]])
+#     R = eulerAnglesToRotationMatrix(0, 0, angle - 15)
+#     newExtrinsic = np.hstack((R, T))
+#     cam_mat = IntrinsicAlley @ newExtrinsic
+#     img_cords_Homogenuous = cam_mat @ np.transpose(homogenouos3DCoords)
+#
+#     for i in range(r * c):
+#         backprojectionMatrix[0][i] = int(np.round(img_cords_Homogenuous[0][i] / img_cords_Homogenuous[2][i]))
+#         backprojectionMatrix[1][i] = int(np.round(img_cords_Homogenuous[1][i] / img_cords_Homogenuous[2][i]))
+#
+#     newIm = np.zeros(np.shape(img1))
+#     backprojectionMatrix = np.transpose(backprojectionMatrix)
+#     k = 0
+#     for i in range(r):
+#         for j in range(c):
+#             m, n = backprojectionMatrix[k]
+#             if n >= c or m >= r or n < 0 or m < 0:
+#                 pass
+#             else:
+#                 newIm[i][j] = img1[int(m)][int(n)]
+#             k = k + 1
+#
+#     # cv_io.imshow(depthMapAlley)
+#     # cv2.imshow("origin", img1)
+#     cv2.imwrite("im " + str(imNumber) + ".jpg", newIm)
+#     print("image number: " + str(imNumber))
+#     imNumber = imNumber + 1
