953a260468ea57b93af0e1aa89337c0b960caff0
sagigvili <57597612+sagigvili@users.noreply.github.com>
2021-06-01 19:10:21 +0300

    Q1 - need to calculate Epi. dist. better again

--- Assignment 2/Epipolar Geometry/7pointAlgo.py
+++ Assignment 2/Epipolar Geometry/7pointAlgo.py
@@ -26,13 +26,13 @@ img2 = cv.imread("Resources/im_courtroom_00089_right.jpg")
 # Court Points
 pts1 = [(208.53225806451613, 27.758064516129025), (395.3064516129032, 231.9516129032258), (506.5967741935484, 155.5),
         (614.016129032258, 194.20967741935488), (736.9193548387096, 73.24193548387098), (850.1451612903226, 215.5),
-        (793.0483870967741, 337.43548387096774), (648.8548387096774, 433.241935483871),
+        (793.0483870967741, 337.43548387096774),
         (404.01612903225805, 443.88709677419354), (28.532258064516128, 310.33870967741933),
         (424.3387096774194, 280.33870967741933)]
 
 pts2 = [(265.6290322580645, 15.177419354838662), (389.5, 171.95161290322574), (476.5967741935484, 106.14516129032256),
         (566.5967741935484, 134.20967741935488), (661.4354838709677, 45.17741935483866),
-        (740.7903225806451, 144.85483870967744), (717.5645161290322, 221.30645161290323),
+        (740.7903225806451, 144.85483870967744),
         (645.9516129032259, 285.1774193548387), (535.6290322580645, 284.2096774193548),
         (284.98387096774195, 225.17741935483872), (452.4032258064516, 208.72580645161293)]
 
@@ -51,7 +51,7 @@ pts2 = [(265.6290322580645, 15.177419354838662), (389.5, 171.95161290322574), (4
 pts1 = np.int32(pts1)
 pts2 = np.int32(pts2)
 
-F, mask = cv.findFundamentalMat(pts1, pts2, cv.FM_8POINT)
+F, mask = cv.findFundamentalMat(pts1, pts2, cv.FM_7POINT)
 
 # We select only inlier points
 pts1 = pts1[mask.ravel() == 1]
@@ -61,7 +61,7 @@ pts2 = pts2[mask.ravel() == 1]
 def compute_Correspond_Epilines(points1, points2, F):
     epi_lines1 = []
     epi_lines2 = []
-    for i in range(len(pts1)):
+    for i in range(len(points1)):
         a = np.ones((3, 1))
         a[0][0] = points1[i][0]
         a[1][0] = points1[i][1]
@@ -93,9 +93,33 @@ results_algeb = []
 for pt1, pt2 in zip(pts1, pts2):
     results_algeb.append(np.transpose(np.array([pt2[0], pt2[1], 1])) @ F @ np.array([pt1[0], pt1[1], 1]))
 
+# errors2.append(
+#     power(dis1/sqrt(power(fx[0], 2) + power(fx[1], 2)), 2)
+#     +
+#     power(dis2/sqrt(power(fx[0], 2) + power(fx[1], 2)), 2))
+
+results_epi = []
+for i in range(len(pts1)):
+    x_homogen = np.ones((3, 1))
+    x_homogen[0] = pts1[i][0]
+    x_homogen[1] = pts1[i][1]
+
+    x_tag_homogen = np.ones((3, 1))
+    x_tag_homogen[0] = pts2[i][0]
+    x_tag_homogen[1] = pts2[i][1]
+
+    Fx = F @ x_homogen
+    alegb_dis1 = abs(np.transpose(x_tag_homogen) @ Fx)
+
+    F_t_x_tag = np.transpose(F) @ x_tag_homogen
+    alegb_dis2 = abs(np.transpose(x_homogen) @ F_t_x_tag)
+
+    final_d = ((alegb_dis1 ** 2) + (alegb_dis2 ** 2)) / ((Fx[0] ** 2) + (Fx[1] ** 2))
+    results_epi.append(final_d)
+
 print("The algebraic distance is {}".format(np.average(np.abs(results_algeb))))
 
-#print("The Epipolar distance for right image is {}".format(np.average(results_epi)))
+print("The Epipolar distance for right image is {}".format(np.average(results_epi)))
 
 im1, im2 = drawlines(img1, img2, lines1, pts1, pts2)
 im3, im4 = drawlines(img2, img1, lines2, pts2, pts1)
